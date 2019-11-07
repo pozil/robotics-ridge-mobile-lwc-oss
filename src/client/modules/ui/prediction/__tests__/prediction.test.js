465a035670603fa6631d50681f14b49f5e65e3eb
Heather Dykstra <heather.dykstra@colorado.edu>
2019-11-07 14:27:01 -0700

    Fixed tests!

--- src/client/modules/ui/prediction/__tests__/prediction.test.js
+++ src/client/modules/ui/prediction/__tests__/prediction.test.js
@@ -2,25 +2,24 @@ import { createElement } from 'lwc';
 import Prediction from 'ui/prediction';
 
 // Sample data
-const prediction_success = [
-    {
-        probabilities: {
+const prediction_success = {
+    probabilities: [
+        {
             probability: 0.99901164,
             label: 'Paper',
             boundingBox: { minY: 463, minX: 307, maxY: 626, maxX: 426 }
         }
-    }
-];
-/**const prediction_no_bounding = [
-    {
-        probabilities: {
+    ]
+};
+const prediction_no_bounding = {
+    probabilities: [
+        {
             probability: 0.7761964,
             label: 'paper',
             boundingBox: null
         }
-    }
-];
-const prediction_no = [];*/
+    ]
+};
 
 describe('display-a-prediction', () => {
     afterEach(() => {
@@ -31,51 +30,36 @@ describe('display-a-prediction', () => {
     });
 
     it('does not render prediction tiles by default', () => {
-        // Create initial element
         const element = createElement('c-prediction', {
             is: Prediction
         });
         document.body.appendChild(element);
 
-        const isStrengthIcon = element.shadowRoot.querySelectorAll(
-            'c-strength-icon'
-        );
-        expect(isStrengthIcon.length).toBe(0);
-        const div = element.shadowRoot.querySelector('div');
-        expect(div.textContent).toBe('No result');
+        const paragraphs = element.shadowRoot.querySelectorAll('p');
+        expect(paragraphs.length).toBe(2);
     });
 
-    it('renders data with predictions ', () => {
+    it('renders data with predictions and no bounding', () => {
         const element = createElement('c-prediction', {
             is: Prediction
         });
-        element.rawPrediction = prediction_success;
-        console.log(element.rawPrediction);
+        element.prediction = prediction_no_bounding;
         document.body.appendChild(element);
-        console.log(element);
-        //Not a valid test
-        //Need to test if the table renders or not, but not sure I'm even passing the data correctly.
-        const p = element.shadowRoot.querySelector('p');
-        expect(p.textContent).toBe('Prediction:');
+        return Promise.resolve().then(() => {
+            const data = element.shadowRoot.querySelectorAll('td');
+            expect(data.length).toBe(3);
+        });
     });
 
-    /**it('renders data with predictions and no bounding', () => {
-        
-        const element = createElement('c-prediction',{
+    it('renders data with predictions and bounding ', () => {
+        const element = createElement('c-prediction', {
             is: Prediction
         });
+        element.prediction = prediction_success;
         document.body.appendChild(element);
-        prediction_no_bounding;
-        expect(SOMETHING);
+        return Promise.resolve().then(() => {
+            const data = element.shadowRoot.querySelectorAll('td');
+            expect(data.length).toBe(4);
+        });
     });
-
-    it('renders when prediction is empty', () => {
-        const element = createElement('c-prediction',{
-            is: Prediction
-        });
-        document.body.appendChild(element);
-        prediction_no;
-        
-        expect(SOMETHING);
-    });*/
 });
